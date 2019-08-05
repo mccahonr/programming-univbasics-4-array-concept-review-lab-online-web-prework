ee170b8ef19c4799780233e8f033a49176844f5d
Ryan McCahon <mccahonr@yahoo.com>
2019-08-05 18:47:25 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -18,11 +18,11 @@ def find_max_value(array)
 end
 
 def find_min_value(array)
-  min_value = 0
+  x = 0
   array.length.times do |index|
-    if array[index] < min_value
-      min_value = array[index]
+    if array[index] < x
+      x = array[index]
     end
   end
-  min_value
+  x
 end
