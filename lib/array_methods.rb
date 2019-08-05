7c8aafe7242b82bef0b62cd9f8fb6739f939adce
Ryan McCahon <mccahonr@yahoo.com>
2019-08-05 18:41:14 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -9,7 +9,7 @@ end
 
 def find_max_value(array)
   max_value = 0
-  array.length.times do |count|
+  array.length.times do |index|
     if array[index] > max_value
       max_value = array[index]
     end
